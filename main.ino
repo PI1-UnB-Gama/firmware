1288fc16bd459e08465255d3c445691f15c8e36f
kyomi <me@kyomi.dev>
2024-07-10 02:00:35 -0300

    Rewrite the firmware code

--- main.ino
+++ main.ino
@@ -1,185 +1,247 @@
+// #include <SPI.h>
 #include <SD.h>
-#include <SPI.h>
 
-//Definição dos pinos de controle do motor
-#define M1 9 // Pino_Velocidade 1º Motor ( 0 a 255)_ Porta IN2 ponte H; 9
-#define M2 11 //Pino_Velocidade 2º Motor ( 0 a 255) _ Porta IN4 ponte H;11
-#define dir1 8 //Pino_Direção do 1º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN1 ponte H; 8
-#define dir2 10 //Pino_Direção do 2º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN3 ponte H;10
-//Definição dos pinos dos sensores
-#define pin_S1 7
-#define pin_S2 6
+// String msg;
 
-// --- Mapeamento de Hardware ---
-#define   vcc      5
-#define   gnd      4
-#define   pino_D0  3
-#define   analog   2
+// Informações úteis:
+// Para ativar o modo de upload, ative a terceira e quarta chave.
+// Para ativar o modo de execução, ative a primeira e segunda chave.
 
-bool Sensor1 = 0;
-bool Sensor2 = 0;
-File myFile;
+// Definição dos pinos do motor.
+// M1 representa o motor da esquerda e M2 o motor da direita.
+#define M1 11
+#define M2 9
 
-int pinoSS = 53; // Pin 53 para Mega / Pin 10 para UNO
+// Definição dos pinos dos sensores.
+// S1 representa o sensor da esquerda e S2 o sensor da direita.
+#define S1 7
+#define S2 6
 
-float voltage;
+// Definição do pino do amperímetro.
+#define AMP A0
 
-//variável responsável por controlar a velocidade dos motores
-int velocidade = 80;
+// Representa a velocidade do carrinho ao se mover para frente.
+const int velocity = 60;
+// Representa a velocidade do carrinho ao se mover para os lados.
+const int turn_velocity = 50;
+// Representa o tempo de espera para o carrinho. Isso é necessário para
+// que o carrinho tenha tempo de virar antes de continuar a se mover para
+// frente, caso contrário ele se moverá muito pouco para o lado, perderá o
+// contato com a linha e começará a se mover para frente, saindo da curva.
+const int default_delay = 450;
+// Nome do arquivo CSV que será gravado no cartão SD.
+const char filename[] = "data.txt";
 
-volatile int cont = 0;
-int voltas = 0;
-int time = millis();
+// Representa o incremento do ângulo do carrinho ao se mover para os lados.
+// Server apenas para mostrar o ângulo atual do carrinho.
+const int angle_incrementor = 30;
+// Representa o ângulo atual do carrinho.
+int angle = 0;
+// Define se o carrinho terminou de seguir a linha.
+bool finished = false;
 
-// ========================================================================================================
-// --- Variáveis Globais ---
-int           rpm;
-volatile byte pulsos;
-unsigned long timeold;
+// Arquivo de dados que será gravado no cartão SD.
+File data;
 
-//Altere o numero abaixo de acordo com o seu disco encoder
-unsigned int pulsos_por_volta = 20;
+// volatile int cont = 0;
+// int voltas = 0;
+// unsigned long time;
 
+//
+// Algoritmo para seguir a linha e determinar o ângulo atual do carrinho.
+//
+void follow_line() {
+  bool sensor_left = digitalRead(S1), sensor_right = digitalRead(S2);
 
-// ========================================================================================================
-// --- Interrupção ---
-void contador()
-{
-    //Incrementa contador
-    pulsos++;
+  if ((sensor_left == 0) && (sensor_right == 0)) {
+    // Serial.println("Nenhuma linha detectada. Seguindo em frente.");
+
+    analogWrite(M1, velocity);
+    analogWrite(M2, velocity);
+  }
+
+  if ((sensor_left == 1) && (sensor_right == 0)) {
+    // Serial.println("Linha detectada à esquerda. Desativando motor esquerdo.");
+
+    analogWrite(M1, 0);
+    analogWrite(M2, turn_velocity);
+
+    angle -= angle_incrementor;
+
+    if (angle < 0)
+      angle = 360 + angle;
+  }
+
+  if ((sensor_left == 0) && (sensor_right == 1)) {
+    // Serial.println("Linha detectada à direita. Desativando motor direito.");
+
+    analogWrite(M1, turn_velocity);
+    analogWrite(M2, 0);
+
+    angle += angle_incrementor;
+  }
+
+  if ((sensor_left == 1) && (sensor_right == 1)) {
+    // Serial.println("Linha detectada em ambos os lados. Parando.");
+
+    analogWrite(M1, 0);
+    analogWrite(M2, 0);
+
+    finished = true;
+    Serial.println("Carrinho terminou de seguir a linha.");
+  }
+
+  angle = angle % 360;
+
+  // Serial.print("Ângulo atual: ");
+  // Serial.println(angle);
 }
 
-void setup(){
-    //Setamos os pinos de controle dos motores como saída
-    pinMode(M1, OUTPUT);
-    pinMode(M2, OUTPUT);
-    pinMode(dir1, OUTPUT);
-    pinMode(dir2, OUTPUT);
-    //Setamos a direção inicial do motor como 0, isso fará com que ambos os motores girem para frente
-    digitalWrite(dir1, LOW);
-    digitalWrite(dir2, LOW);
-    //Setamos os pinos dos sensores como entrada
-    pinMode(pin_S1, INPUT);
-    pinMode(pin_S2, INPUT);
+//
+// Salvar dados no cartão SD.
+//
+void save_data(unsigned long timestamp, int angle, int amperage) {
+  data.print(timestamp);
+  data.print(",");
+  data.print(angle);
+  data.print(",");
+  data.print(amperage);
+  data.println();
 
-    Serial.begin(9600);
-
-    pinMode(vcc,    OUTPUT);
-    pinMode(gnd,    OUTPUT);
-    pinMode(pino_D0, INPUT);
-    pinMode(analog,  INPUT);
-
-    digitalWrite(vcc, HIGH);
-    digitalWrite(gnd,  LOW);
-
-    //Interrupcao 1 - pino digital 3
-    //Aciona o contador a cada pulso
-    attachInterrupt(1, contador, FALLING);
-    pulsos = 0;
-    rpm = 0;
-    timeold = 0;
-
-    // put your setup code here, to run once:
-    pinMode(3, INPUT);
-    attachInterrupt(digitalPinToInterrupt(3), interrupcao, RISING);
-    Serial.begin(9600);
-    Serial.println(" - Inicio - ");
-
-    // Executado uma vez quando ligado o Arduino
-
-    Serial.begin(9600); // Define BaundRate
-    pinMode(pinoSS, OUTPUT); // Declara pinoSS como saída
-
-    if (SD.begin()) { // Inicializa o SD Card
-        Serial.println("SD Card pronto para uso."); // Imprime na tela
-    } else {
-        Serial.println("Falha na inicialização do SD Card.");
-        return;
-    }
-
-    myFile = SD.open("dados.csv", FILE_WRITE); // Cria / Abre arquivo .txt
-
-    if (myFile) { // Se o Arquivo abrir imprime:
-        Serial.println("Escrevendo no Arquivo .csv"); // Imprime na tela
-        myFile.println("Usinainfo 1, 2 ,3 ..."); // Escreve no Arquivo
-        myFile.println(voltas);
-        myFile.println("/n /n");
-        myFile.println(cont);
-        myFile.println("/n /n");
-        myFile.println(time);
-
-        myFile.close(); // Fecha o Arquivo após escrever
-        Serial.println("Terminado."); // Imprime na tela
-    } else {     // Se o Arquivo não abrir
-        Serial.println("Erro ao Abrir Arquivo .csv"); // Imprime na tela
-    }
-
-    myFile = SD.open("Dados.csv"); // Abre o Arquivo
-
-    if (myFile) {
-        Serial.println("Conteúdo do Arquivo:"); // Imprime na tela
-
-        while (myFile.available()) { // Exibe o conteúdo do Arquivo
-            Serial.write(myFile.read());
-        }
-
-        myFile.close(); // Fecha o Arquivo após ler
-    } else {
-        Serial.println("Erro ao Abrir Arquivo .csv"); // Imprime na tela
-    }
+  Serial.print("Dados salvos: ");
+  Serial.print(timestamp);
+  Serial.print(",");
+  Serial.print(angle);
+  Serial.print(",");
+  Serial.print(amperage);
+  Serial.println();
 }
 
-void loop(){
-    //Neste processo armazenamos o valor lido pelo sensor na variável que armazena tais dados.
-    Sensor1 = digitalRead(pin_S1);
-    Sensor2 = digitalRead(pin_S2);
-    //Aqui está toda a lógica de comportamento do robô: Para a cor branca atribuímos o valor 0 e, para a cor preta, o valor 1.
-    if((Sensor1 == 0) && (Sensor2 == 0)) { // Se detectar na extremidade das faixas duas cores brancas
-        analogWrite(M1, velocidade); // Ambos motores ligam na mesma velocidade
-        analogWrite(M2, velocidade);
-    }
-    if((Sensor1 == 1) && (Sensor2 == 0)) { // Se detectar um lado preto e o outro branco
-        analogWrite(M2, 0); // O motor 1 desliga
-        analogWrite(M1, velocidade); // O motor 2 fica ligado, fazendo assim o carrinho virar
-    }
-    if((Sensor1 == 0) && (Sensor2 == 1)) { // Se detectar um lado branco e o outro preto
-        analogWrite(M2, velocidade); //O motor 1 fica ligado
-        analogWrite(M1, 0); // O motor 2 desliga, fazendo assim o carrinho virar no outro sentido
-    }
+void setup() {
+  Serial.begin(9600);
+//   Serial3.begin(9600);
 
-    //Atualiza contador a cada segundo
-    if (millis() - timeold >= 1000) {
-        //Desabilita interrupcao durante o calculo
-        detachInterrupt(1);
-        rpm = (60 * 1000 / pulsos_por_volta ) / (millis() - timeold) * pulsos;
-        timeold = millis();
-        pulsos = 0;
+//   pinMode(pinoCS, OUTPUT);
+//   delay(500);
 
-        //Mostra o valor de RPM no serial monitor
-        Serial.print("RPM = ");
-        Serial.println(rpm, DEC);
-        //Habilita interrupcao
-        attachInterrupt(1, contador, FALLING);
-    }
-    unsigned int x=0;
-    float AcsValue=0.0,Samples=0.0,AvgAcs=0.0,AcsValueF=0.0;
-    for (int x = 0; x < 10; x++)          //Get 10 samples
-    {
-        AcsValue = analogRead(A0);           //Read current sensor values
-        Samples = Samples + AcsValue;        //Add samples together
-        delay (3);                           // let ADC settle before next sample 3ms
-    }
-    AvgAcs=Samples/10.0;                   //Taking Average of Samples
-    voltage=AvgAcs*(5.0 / 1024.0);         //((AvgAcs * (5.0 / 1024.0)) is converitng the read voltage in 0-5 volts
-    Serial.print("Raw Voltage:");
-    Serial.print(voltage);
-    AcsValueF = (2.5 - voltage)*1000/0.185; //2.5 is offset,,,   0.185v is rise in output voltage when 1A current flows at input
-    Serial.print("\t");
-    Serial.print("Motor Current :");
-    Serial.print(AcsValueF);               //Print the read current on Serial monitor
-    Serial.println(" mA");
+  pinMode(M1, OUTPUT);
+  pinMode(M2, OUTPUT);
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+//   digitalWrite(dir1, LOW);
+//   digitalWrite(dir2, LOW);
+
+//   pinMode(3, INPUT);
+//   attachInterrupt(digitalPinToInterrupt(3), interrupcao, RISING);
+
+  if (!SD.begin()) {
+    Serial.println("Falha ao iniciar o cartão micro SD.");
+    return;
+  }
+
+  Serial.println("Cartão micro SD inicializado com sucesso.");
+
+  // Remove o arquivo de dados antigo, caso exista.
+  int removed = SD.remove(filename);
+
+  if (removed)
+    Serial.println("Arquivo de dados antigo removido com sucesso.");
+  else
+    Serial.println("Falha ao remover o arquivo de dados antigo (o arquivo possivelmente não existe).");
+
+  // Cria um novo arquivo de dados.
+  data = SD.open(filename, FILE_WRITE);
+
+  if (data)
+    Serial.println("Arquivo de dados criado com sucesso.");
+  else
+    Serial.println("Falha ao criar o arquivo de dados.");
 }
 
-void interrupcao(){
-    cont++;
+void loop() {
+  if (finished) {
+    data.close();
+    return;
+  }
+
+  //
+  // Algoritmo para seguir a linha e obter o ângulo do carrinho.
+  //
+  follow_line();
+
+  //
+  // Obtendo o timestamp atual.
+  //
+  unsigned long timestamp = millis();
+
+  //
+  // Obtendo a leitura do amperímetro em miliamperes.
+  //
+  int amperage = analogRead(AMP);
+
+  //
+  // Salvando dados no cartão SD.
+  //
+  save_data(timestamp, angle, amperage);
+
+  //
+  // Aguardando um tempo para que o carrinho tenha tempo de virar antes de
+  // continuar a se mover para frente.
+  //
+  delay(default_delay);
+
+//   delay(1000);
+//   detachInterrupt(digitalPinToInterrupt(3));
+//   voltas = (cont * 3);
+//   time = millis();
+//   Serial.println(voltas);
+//   cont = 0;
+//   attachInterrupt(digitalPinToInterrupt(3), interrupcao, RISING);
+
+//   if (Serial3.available()) {
+//     char data = Serial3.read();
+//     Serial.print(cont);
+//     Serial.print(voltas);
+//     Serial.print(time);
+//     msg += data;
+
+//     if (data == 13) {
+//       msg = "";
+//     }
+//     if (msg.indexOf("LED_ON") > 0) {
+//       digitalWrite(LED_BUILTIN, HIGH);
+//     } else if (msg.indexOf("LED_OFF") > 0) {
+//       digitalWrite(LED_BUILTIN, LOW);
+//     }
+//   }
+
+//   numeroLeitura++;
+//   leituraLDR = analogRead(pinoLDR);
+
+//   unsigned long timestamp = millis(); // Obtém o timestamp atual
+
+//   Serial.print("Leitura ");
+//   Serial.print(numeroLeitura);
+//   Serial.print(": ");
+//   Serial.println(leituraLDR);
+
+//   File data = SD.open("arquivo.txt", FILE_WRITE);
+//   if (data) {
+//     data.print(timestamp);
+//     data.print(",");
+//     data.print(Sensor1);
+//     data.print(",");
+//     data.print(Sensor2);
+//     data.print(",");
+//     data.println(leituraLDR);
+//     data.close();
+//   } else {
+//     Serial.println("Erro ao abrir o arquivo");
+//   }
+
+//   delay(5000);
 }
+
+// void interrupcao() {
+//   cont++;
+// }
